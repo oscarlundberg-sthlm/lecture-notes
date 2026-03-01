586f06eb585894c1a01563d1a0e9f10a581a3f80
oscarlundberg-sthlm <oscarlundberg@hotmail.com>
2026-03-01 11:14:07 +0100

    clean up

--- start-dev.sh
+++ start-dev.sh
@@ -11,19 +11,3 @@ EOF
 echo "🤖 Starting Electron...";
 
 yarn workspace lecture-notes-electron start;
-
-# echo "🤖 Starting Next.js in a new terminal window...";
-
-# osascript <<EOF
-# tell application "Terminal"
-#     do script "cd $currentPath/next-js && yarn dev"
-# end tell
-# EOF
-
-# echo "🤖 Starting Electron...";
-
-# cd $currentPath/electron && yarn start;
-
-# echo "This doesn't work yet.";
-# echo "For Next.js development, run it in the browser.";
-# echo "Later incorporate Electron, in the build process of the app.";
