5298a0809c78e309483cf9691ae726204c39ce74
oscarlundberg-sthlm <oscarlundberg@hotmail.com>
2026-03-07 15:02:15 +0100

    changed on focus scroll behaviour

--- next-js/src/components/NotesEditor.tsx
+++ next-js/src/components/NotesEditor.tsx
@@ -20,7 +20,7 @@ export default function NotesEditor({ value, onSave, className }: Props) {
     editorProps: {
       attributes: {
         class:
-          "h-full p-2 [&_ol_li]:list-decimal [&_ul_li]:list-disc [&_ol]:my-2 [&_ol]:ps-7 [&_ul]:my-2 [&_ul]:ps-6 mx-auto rounded-lg bg-white shadow-md border border-gray-300",
+          "h-full p-2 [&_ol_li]:list-decimal [&_ul_li]:list-disc [&_ol]:my-2 [&_ol]:ps-7 [&_ul]:my-2 [&_ul]:ps-6 mx-auto rounded-lg bg-white shadow-md border border-gray-300 scroll-mt-4",
       },
     },
     onFocus(e) {
@@ -28,7 +28,7 @@ export default function NotesEditor({ value, onSave, className }: Props) {
         return;
       }
       if (e.event.target instanceof HTMLElement) {
-        e.event.target.scrollIntoView({ behavior: "smooth", block: "center" });
+        e.event.target.scrollIntoView({ behavior: "instant", block: "start" });
       }
     },
     onUpdate({ editor }) {
@@ -45,42 +45,6 @@ export default function NotesEditor({ value, onSave, className }: Props) {
 
   return (
     <div className={className}>
-      {/* Toolbar */}
-      {/* <div className="flex gap-2 mb-2 border-b pb-2">
-        <button
-          onClick={() => editor.chain().focus().toggleBold().run()}
-          className={`px-2 py-1 rounded ${
-            editor.isActive("bold") ? "bg-gray-300" : ""
-          }`}
-        >
-          Bold
-        </button>
-
-        <button
-          onClick={() => editor.chain().focus().toggleItalic().run()}
-          className={`px-2 py-1 rounded ${
-            editor.isActive("italic") ? "bg-gray-300" : ""
-          }`}
-        >
-          Italic
-        </button>
-
-        <button
-          onClick={() => editor.chain().focus().undo().run()}
-          className="px-2 py-1 rounded"
-        >
-          Undo
-        </button>
-
-        <button
-          onClick={() => editor.chain().focus().redo().run()}
-          className="px-2 py-1 rounded"
-        >
-          Redo
-        </button>
-      </div> */}
-
-      {/* Editor */}
       <EditorContent editor={editor} className="grow" />
     </div>
   );
